9f0a2dc2503c1e390823ce85e0729f4cdb531702
abaidullah696 <abaidullah696@gmail.com>
2024-07-04 20:34:29 +0500

    Timeline rough practice

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ gsap.to(".abc", {
     delay:2,
     backgroundColor:"red ",
     borderRadius:"0",
+    repeat:1,               //this property is use to repeat the animations if we put value -1 then it work infinite time
+    yoyo:true,              // YOYO property is use to animate like to and from both at a time 
 })
 //To animate final to initial point
 gsap.from("h1", {
@@ -15,5 +17,32 @@ gsap.from("h1", {
     y:30,
     delay:1,
     stagger:1,               // Stagger is use to animate all elements one by one
+    repeat:-1,
 })
-//
+
+
+// Know we learn about timeline it is use to animate element one after another once the 1st complete second comes to animate and vise versa
+
+var tl = gsap.timeline();
+
+
+tl.to(".box1",{
+    rotate:360,
+    x:600,
+    duration:2,
+    backgroundColor:"green",
+})
+tl.to(".box2",{
+    rotate:180,
+    x:600,
+    duration:2,
+    backgroundColor:"green",
+    borderRadius:"50%"
+})
+tl.to(".box3",{
+    rotate:90,
+    x:600,
+    duration:2,
+    backgroundColor:"green",
+    borderRadius:"50px"
+})
