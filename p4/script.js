a4044fa342408bd33eec5c302b381d242fcd07f8
abaidullah696 <abaidullah696@gmail.com>
2024-07-06 16:59:25 +0500

    curser functionality in a card

--- p4/script.js
+++ p4/script.js
@@ -1,12 +1,37 @@
 var main = document.querySelector("#main")
 var curser = document.querySelector("#curser")
+var image = document.querySelector("#image")
 
 
+
+// Track mouse movment code
 main.addEventListener("mousemove", function (dets) {
     gsap.to(curser, {
         x:dets.x,
         y:dets.y,
         duration:0.1,
         ease:"back.out"
+    })
+})
+
+
+//Mouse enter code
+image.addEventListener("mouseenter", function () {
+    console.log("Hello")
+    curser.innerHTML = "View More";
+    gsap.to(curser, {
+        scale:4,
+        backgroundColor:"#ffffff8a"
+    })
+})
+
+
+//Mouse leave code
+image.addEventListener("mouseleave", function () {
+    console.log("world")
+    curser.innerHTML = "";
+    gsap.to(curser, {
+        scale:0,
+        backgroundColor:"#fff"
     })
 })
