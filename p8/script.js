fd2aca11231079a337468a7bfcdce7545b28cbe4
abaidullah696 <abaidullah696@gmail.com>
2024-07-19 20:36:06 +0500

    Later work for scrolltrigger

--- p8/script.js
+++ p8/script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function page1Animation() {
+    var tl = gsap.timeline();
 
 tl.from("nav h1, nav h2, nav button", {
     y:-30,
@@ -14,6 +15,7 @@ tl.from("#cntr-left h1, #cntr-left p, #cntr-left button", {
 })
 tl.from("#cntr-right img", {
     opacity:0,
+    x:200
 
 }, "-=0.3")             //control the delay in time line
 tl.from("#section1bottom img",{
@@ -21,3 +23,35 @@ tl.from("#section1bottom img",{
     opacity:0,
     stagger:0.3
 })
+}
+
+// page1Animation()
+
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        scroller:"body",
+        trigger: ".section2",
+        markers: true,
+        start: "top 50%", // Trigger animation when top of trigger element hits 50% of viewport height
+        end:"top 80%",
+        scrub: 2         // Scrub animation over 2 units of scroll
+    }
+});
+
+tl2.from(".services h3", {
+    y: -300,
+    opacity: 0,
+    duration: 1,
+    delay: 2
+});
+
+
+
+
+
+
+
+
+
+// page1Animation()
+// page2Animation()
